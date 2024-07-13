10501d94afc9e382da884f82b0f596028a55bc24
varshini <varshininidagundi2001@gmail.com>
2024-07-13 15:42:12 +0530

    first commit

---
+++ src/components/Board/Board.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Square from '../Square/Square';
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  const isDraw = squares.every(square => square !== null) && !winner;
+  const status = winner
+    ? 'Winner: ' + winner
+    : isDraw
+    ? 'Draw'
+    : 'Next player: ' + (xIsNext ? 'X' : 'O');
+
+  function renderSquare(i) {
+    return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />;
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
